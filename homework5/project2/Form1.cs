4d6cd01c5a9fa713d64b8283bc89d1fe3f3ce9d2
xl Deer <YAOXL819@outlook.com>
2018-10-16 20:32:24 +0800

    20181016

--- homework5/project2/Form1.cs
+++ homework5/project2/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace project2
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
@@ -16,83 +16,126 @@ namespace project2
         {
             InitializeComponent();
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            if (graphics != null) graphics = this.CreateGraphics();
+            if (graphics == null) graphics = this.CreateGraphics();
             drawTree(10, 200, 310, 100, -Math.PI / 2);
         }
 
         private Graphics graphics;
+        
+        public double temp1 = 40;
+        public double temp2 = 30;
 
-        static double temp1 = 30,temp2 = 20;
-        double th1 = temp1 * Math.PI / 180;
-        double th2 = temp2 * Math.PI / 180;
-        double per1 = 0.6;
-        double per2 = 0.7;
+        public double per1 = 0.6;
+        public double per2 = 0.7;
+        public int LineWidth = 1;
+
+        Random rnd = new Random();
+        double rand()
+        {
+            return rnd.NextDouble();
+        }
         void drawTree(int n, double x0, double y0, double leng, double th)
         {
             if (n == 0) return;
 
-            double x1 = x0 + leng * Math.Cos(th);
+            double th1 = temp1 * Math.PI / 180;
+            double th2 = temp2 * Math.PI / 180;
+
+            double x1 = x0 + leng * Math.Cos(th); //设置分支1
             double y1 = y0 + leng * Math.Sin(th);
 
-            double x2 = x0 + leng * Math.Sin(th);
-            double y2 = y0 + leng * Math.Cos(th); 
+            double x2 = x1; //设置分支2
+            double y2 = y1;
 
-            drawLine(x0, y0, x1, y1);
-            drawLine(x0, y0, x2, y2);
+            if (checkBox1.Checked)
+            {
+                drawLine(x0 + rand(), y0 + rand(), x1 + rand(), y1 + rand());
+                drawLine(x0 + rand(), y0 + rand(), x2 + rand(), y2 + rand());
 
-            drawTree(n - 1, x1, y1, per1 * leng, th + th1);
-            drawTree(n - 1, x2, y2, per2 * leng, th - th2);
+                drawTree(n - 1, x1 + rand(), y1 + rand(), per1 * leng * (0.5 + rand()), th + th1 * (0.5 + rand()));
+                drawTree(n - 1, x2 + rand(), y2 + rand(), per2 * leng * (0.4 + rand()), th - th2 * (0.5 + rand()));
+                if (rand() > 0.6)
+                {
+                    drawLine(x0 + rand(), y0 + rand(), x1 + rand(), y1 + rand());
+                    drawLine(x0 + rand(), y0 + rand(), x2 + rand(), y2 + rand());
+
+                    drawTree(n - 1, x1 + rand(), y1 + rand(), per2 * leng * (0.4 + rand()), th - th2 * (0.5 + rand()));
+                    drawTree(n - 1, x1 + rand(), y1 + rand(), per2 * leng * (0.4 + rand()), th - th2 * (0.5 + rand()));
+                }
+                   
+
+            }
+            else
+            {
+                drawLine(x0, y0 , x1, y1);
+                drawLine(x0, y0 , x2, y2);
+
+                drawTree(n - 1, x1, y1, per1 * leng, th + th1);
+                drawTree(n - 1, x2, y2, per2 * leng, th - th2);
+            }
         }
 
         void drawLine(double x0, double y0, double x1, double y1)
         {
             if (radioButton1.Checked)
-            { 
-                graphics.DrawLine(Pens.Black,
-                (int)x0, (int)y0, (int)x1, (int)y1);
-            }
-            else if(radioButton2.Checked)
             {
-                graphics.DrawLine(Pens.Blue,
-                    (int)x0, (int)y0, (int)x1, (int)y1);
+                Pen temp = new Pen(Color.Black, LineWidth);
+                graphics.DrawLine(temp, (int)x0, (int)y0, (int)x1, (int)y1);
             }
-            else if(radioButton3.Checked)
+            else if (radioButton2.Checked)
             {
-                graphics.DrawLine(Pens.Red,
-                    (int)x0, (int)y0, (int)x1, (int)y1);
+                Pen temp = new Pen(Color.Blue, LineWidth);
+                graphics.DrawLine(temp, (int)x0, (int)y0, (int)x1, (int)y1);
             }
-            else if(radioButton4.Checked)
+            else if (radioButton3.Checked)
             {
-                graphics.DrawLine(Pens.Green,
-                    (int)x0, (int)y0, (int)x1, (int)y1);
+                Pen temp = new Pen(Color.Brown, LineWidth);
+                graphics.DrawLine(temp, (int)x0, (int)y0, (int)x1, (int)y1);
             }
         }
-
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             try
             {
-                temp1 = Convert.ToDouble(Console.ReadLine());
+                temp1 = Convert.ToDouble(textBox1.Text);
             }
             catch
             {
                 return;
             }
-        }   //角度一
+        }  //角度一
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                LineWidth = Convert.ToInt32(textBox3.Text);
+            }
+            catch{return;}
+        }  //线宽
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
             try
             {
-                temp2 = Convert.ToDouble(Console.ReadLine());
+                temp2 = Convert.ToDouble(textBox2.Text);
             }
             catch
             {
                 return;
             }
         }   //角度二
+
     }
 }
--- homework5/project2/Properties/AssemblyInfo.cs
+++ homework5/project2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("project2")]
+[assembly: AssemblyTitle("WindowsFormsApp1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("project2")]
+[assembly: AssemblyProduct("WindowsFormsApp1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("4eb4c1f0-1af0-4562-8099-93fa779523ad")]
+[assembly: Guid("7bdff5c7-4885-4565-a883-8a230faa80c3")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
