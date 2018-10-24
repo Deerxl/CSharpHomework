91dcf10f28a4d38670e81cf7682f0d6c2e8da183
xl Deer <YAOXL819@outlook.com>
2018-10-24 20:12:39 +0800

    写了一周

---
+++ homework6/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using project1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string id = "20181022001";
+        string name = "Alu";
+        string item = "flower";
+        int num = 2;
+        double price = 99.9;
+
+        [TestMethod]
+        public void AddNewOrderTest()
+        {
+            Order temp = new Order(id, name);
+            OrderDetails orderDetails = new OrderDetails(item, num, price);
+           //List<OrderDetails> list = new List<OrderDetails>();
+            //temp.Items = list;
+            temp.Items.Add(orderDetails);
+            //Assert.AreEqual(OrderService.AddNewOrder(id, name),temp);
+
+
+
+            //OrderService temp = new project1.OrderService();
+            // temp.Add(order);
+
+
+            // project1.OrderService.orders.Add();
+            //Assert.IsNull(OrderService.AddNewOrder());
+        }
+    }
+}
